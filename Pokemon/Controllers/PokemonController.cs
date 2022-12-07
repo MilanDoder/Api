e496e229e85c567af3ec4883244fbe72b8d29df1
MilanDoder <mdoderovic5@gmail.com>
2022-12-07 21:49:09 +0100

    Added repository pattern

---
+++ Pokemon/Controllers/PokemonController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonApi.Interfaces;
+using PokemonApi.Models;
+
+namespace PokemonApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+
+        public PokemonController(IPokemonRepository pokemonRepository)
+        {
+            _pokemonRepository = pokemonRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type=typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemon()
+        {
+            var pokemons = _pokemonRepository.GetPokemons();
+
+            if (!ModelState.IsValid) {
+                return BadRequest(pokemons);
+            }
+            return Ok(pokemons);
+
+        }
+    }
+}
